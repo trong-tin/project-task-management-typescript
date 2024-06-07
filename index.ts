724d7f73aabed82e730e2560347e8094269cf57b
trong-tin <trongtinvlyk@gmail.com>
2024-06-07 11:15:19 +0700

    Lấy ra chi tiết công việc

--- index.ts
+++ index.ts
@@ -9,17 +9,27 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 app.get("/tasks", async (req: Request, res: Response) => {
-  const task = await Task.find({
+  const tasks = await Task.find({
     deleted: false,
   });
-  console.log(task);
+  console.log(tasks);
+  res.json({
+    code: 200,
+    tasks,
+  });
+});
 
+app.get("/tasks/detail/:id", async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  const task = await Task.findOne({
+    deleted: false,
+    _id: id,
+  });
   res.json({
     code: 200,
     task,
   });
 });
-
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
 });
