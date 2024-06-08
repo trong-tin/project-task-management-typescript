0d840748df7e9fbefee03d70b8394a97394e7bd3
trong-tin <trongtinvlyk@gmail.com>
2024-06-08 18:46:34 +0700

    Install CORS

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import * as database from "./config/database";
 import mainV1Routes from "./api/v1/routes/indexRoute";
 import bodyParser from "body-parser";
@@ -12,11 +13,18 @@ const app: Express = express();
 const port: number | string = process.env.PORT || 3000;
 
 // parse application/json
-app.use(bodyParser .json());
+app.use(bodyParser.json());
+
+// CORS
+
+// var corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+app.use(cors());
 
 mainV1Routes(app);
-
-
 
 app.listen(port, () => {
   console.log(`App listen on port ${port}`);
